8b8f2a3552bc7a4249233073104ace43811c835d
HanZawNyein <hanzawnyineonline@gmail.com>
2023-03-09 00:40:22 +0630

    upodate reporter

--- __manifest__.py
+++ __manifest__.py
@@ -2,11 +2,17 @@
     "name": "Reporter",
     "author": "Han Zaw Nyein",
     "license": "LGPL-3",
+    "depends": [],
     "data": [
         'security/ir.model.access.csv',
 
         'views/hz_reporter.xml',
         'report/hz_reporter.xml',
         'report/report.xml'
-    ]
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'hz_reporter/static/src/**/*',
+        ]
+    }
 }
